7504fd0eaeee5f85e9391b2ce9ff1cceca493279
Anni <anniiwhere@gmail.com>
2020-08-13 11:32:16 +0200

    Clean code in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,18 @@ import React, { useState } from 'react'
 import './App.css'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
-import { v4 as uuidv4 } from 'uuid'
 
 export default function App() {
   const [todos, setTodos] = useState([])
 
   return (
     <div className="App">
-      <TodoForm onSubmit={handleSubmit} />
+      <TodoForm onCreateTodo={addTodo} />
       <TodoList todos={todos} />
     </div>
   )
 
-  function handleSubmit(event) {
-    event.preventDefault()
-    const form = event.target
-    const input = form.todo
-    const text = input.value
-    setTodos([...todos, { text, id: uuidv4() }])
+  function addTodo(todo) {
+    setTodos([...todos, todo])
   }
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './TodoForm.css'
+import { v4 as uuidv4 } from 'uuid'
 
-export default function TodoForm({ onSubmit }) {
+export default function TodoForm({ onCreateTodo }) {
   return (
-    <form onSubmit={onSubmit} className="TodoForm">
+    <form onSubmit={handleSubmit} className="TodoForm">
       <label htmlFor="todo-input">Add Your Dos</label>
       <div>
         <input id="todo-input" name="todo" />
@@ -11,4 +12,12 @@ export default function TodoForm({ onSubmit }) {
       </div>
     </form>
   )
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = event.target
+    const input = form.todo
+    onCreateTodo({ text: input.value, id: uuidv4() })
+    form.reset()
+    input.focus()
+  }
 }
